eb3b67263107c87ecf2c04475f48712ebc5c65d4
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-06-18 00:08:17 +0900

    Add example file

---
+++ example.c
@@ -0,0 +1,15 @@
+#include "mpfi_legendre_polynomial.h"
+
+
+/**
+ * @note
+ * How to compile:
+ * ```
+ * gcc example.c -Wall -Wextra -lgmp -lmpfr -lmpfi
+ * ```
+ * @version Experimental
+ */
+int main(void)
+{
+    printf("The example completed successfully.\n");
+}
