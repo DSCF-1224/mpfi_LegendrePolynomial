1da943ee30b5f99195b30d0d3b0111e9fd61be5a
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-06-20 07:11:37 +0900

    Add steps to save the results

--- example.c
+++ example.c
@@ -3,6 +3,41 @@
 
 
 /**
+ * @brief
+ * <ol>
+ *   <li>
+ *     Open the file to save the results.<br>
+ *     If opening fails, terminate the execution of this function at that point.
+ *   </li>
+ *   <li>
+ *     Close the file containing the results.
+ *   </li>
+ * </ol>
+ * 
+ * @version Experimental
+ */
+int example_mpfi_LegendrePolynomial_Recursive(const unsigned long degree)
+{
+    char output_filename[FILENAME_MAX];
+
+    snprintf(output_filename, sizeof(output_filename), "degree_%3.3ld.dat", degree);
+
+    FILE *output_filestream = fopen(output_filename, "w");
+
+    if (output_filestream == NULL)
+    {
+        fprintf(stderr, "Failed to open `%s`", output_filename);
+        return EXIT_FAILURE;
+    }
+
+    fclose(output_filestream);
+
+    return EXIT_SUCCESS;
+}
+
+
+
+/**
  * @note
  * How to compile:
  * ```
@@ -15,6 +50,10 @@ int main(void)
     printf( "GMP  VERSION; %s\n", gmp_version        );
     printf( "MPFR VERSION; %s\n", mpfr_get_version() );
     printf( "MPFI VERSION; %s\n", mpfi_get_version() );
+
+    example_mpfi_LegendrePolynomial_Recursive(  24UL );
+    example_mpfi_LegendrePolynomial_Recursive(  53UL );
+    example_mpfi_LegendrePolynomial_Recursive( 113UL );
 
     printf("The example completed successfully.\n");
 }
