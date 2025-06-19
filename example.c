bc8bd7b3212a78d36bd9da42890ae65a6603ba28
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-06-20 00:43:43 +0900

    Add step to print the version of the library being used

--- example.c
+++ example.c
@@ -12,5 +12,9 @@
  */
 int main(void)
 {
+    printf( "GMP  VERSION; %s\n", gmp_version        );
+    printf( "MPFR VERSION; %s\n", mpfr_get_version() );
+    printf( "MPFI VERSION; %s\n", mpfi_get_version() );
+
     printf("The example completed successfully.\n");
 }
